c3c0f8107814475cafb44af5dae3d63ed68a6f8e
SonicAF <32658314+SonicAF@users.noreply.github.com>
2022-11-21 14:53:05 +0400

    Initial commit

---
+++ CS/FilterDuplicateRecords_Unbound/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace FilterDuplicateRecords_Unbound
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Source { get; } = new(Item.GetData(1000));
+    }
+}
