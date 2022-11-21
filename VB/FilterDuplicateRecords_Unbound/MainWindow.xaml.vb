24a5cb1de4e5ab8ec59daa15af6704a4026bc058
DevExpressExampleBot <fakeEmail>
2022-11-21 17:09:16 +0400

    autogenerate VB

---
+++ VB/FilterDuplicateRecords_Unbound/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+Imports System.Windows
+
+Namespace FilterDuplicateRecords_Unbound
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
